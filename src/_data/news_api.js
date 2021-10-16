603a9a131d4bbc5ae373efa51810571ebc784a74
Mziyanda Joko <mziyandajoko@Mziyandas-MacBook-Pro.local>
2021-10-16 16:37:11 +0200

    removes dotenv

--- src/_data/news_api.js
+++ src/_data/news_api.js
@@ -1,8 +1,5 @@
 const axios = require("axios");
 
- 
-// const newsApi = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.API_KEY}`;
-
 module.exports = async function getData(){
     try{
         const response = await axios.get("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=fbbb60ba53334b1b87e80a80714d8888");
