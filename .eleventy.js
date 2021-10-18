e99de6219137cfc1654eab1b83629566045620aa
Joko <clementgeorges@Joko.local>
2021-10-18 14:54:10 +0200

    pulls more data from the api

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const {DateTime} = require("luxon");
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughConfig = ('./src/style.css');
+    // format date
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+      });
+
+    eleventyConfig.addPassthroughCopy("./src/css/*.css");
     return {
         dir:{
             input: "src",
-            output: "public"
+            output: "public",
         },
     };
 }
